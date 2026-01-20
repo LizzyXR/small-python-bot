367ac94c785c63ee052ac6fb782a2edb3d8d9883
Lizzy <36128102+LizzyXR@users.noreply.github.com>
2026-01-20 15:59:20 +0100

    bleh

--- future_logger.py
+++ future_logger.py
@@ -1,5 +1,5 @@
 from logging.handlers import RotatingFileHandler
-
+# i don't intend on using this unless i'm hosting it somehwere 24/7 but why would i
 def setup_logging():
     handler = RotatingFileHandler("discord.log", maxBytes=5_000_000, backupCount=3)
     logging.basicConfig(level=logging.INFO, handlers=[handler])
